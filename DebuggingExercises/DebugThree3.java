a7fb6188461d96757b9a6cbdd9c46b5aaf00c8c4
Ballan9 <allan.serrurier@my.jcu.edu.au>
2017-08-18 10:05:13 +1000

    started first task

--- DebuggingExercises/DebugThree3.java
+++ DebuggingExercises/DebugThree3.java
@@ -1,5 +1,5 @@
 // This application gets a user's name and displays a greeting
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class DebugThree3
 {
    public static void main(String args[])
