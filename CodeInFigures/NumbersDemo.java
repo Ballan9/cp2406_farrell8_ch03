c6ae92936ba0435ebf3d96e0c5565a721e7caf61
Ballan9 <allan.serrurier@my.jcu.edu.au>
2017-08-18 10:33:13 +1000

    started first task

--- CodeInFigures/NumbersDemo.java
+++ CodeInFigures/NumbersDemo.java
@@ -1,32 +1,33 @@
-package CodeInFigures;
-
 import javax.swing.*;
 
-public class NumbersDemo
-    {
-        public static void main(String[] args) {
+public class NumbersDemo {
+    public static void main(String[] args) {
            int number1 = GetNum1();
            int number2 = GetNum2();
            numberBy2 = displayTwiceTheNumber(number1, number2);
            numberUp5m = displayPlusFive();
            numberSquared = displayNumberSquared();
 
-        }
-        public static int GetNum1() {
+    }
+    public static int GetNum1() {
             String number1str = JOptionPane.showInputDialog(null,  "Please enter your first number");
             Integer number1 = Integer.parseInt(number1str);
             return number1;
     }
-        public static int GetNum2() {
+    public static int GetNum2() {
             String number2str = JOptionPane.showInputDialog(null,  "Please enter your first number");
             Integer number2 = Integer.parseInt(number2str);
             return number2;
-        }
-        public static int displayTwiceTheNumber(Integer number1, number2);
+    }
+    public static int displayTwiceTheNumber(Integer number1, Integer number2) {
             number1By2 = number1 * 2;
             number2By2 = number2 * 2;
-            JOptionPane.ShowOutputDialog(null, "Your first number squared is: " + numberBy2 + " Your second number squared is: " + number2By1 );
-            return number1, number 2;
+            numberBy2 = JOptionPane.ShowOutputDialog(null, "Your first number squared is: " + number1By2 + " Your second number squared is: " + number2By1);
+            return number1;
+            return number2;
+            return numberBy2;
+    }
+    //public static int
 
 
 }
